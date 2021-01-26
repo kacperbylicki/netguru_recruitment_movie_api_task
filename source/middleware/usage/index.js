08e8ec3d2190215f113e5f1bbbec928c33bdf731
Kacper Bylicki <kacperbylicki@MacBook-Pro-Kacper.local>
2021-01-26 20:13:49 +0100

    Add exepction if request body not provided. Move exepctions out of try-catch block. Refactor role checking conditional statements.

--- source/middleware/usage/index.js
+++ source/middleware/usage/index.js
@@ -1,18 +1,34 @@
-const { getUsage, setUsage, increaseUsage, RedisError } = require("../store");
+const { 
+    getUsage, 
+    setUsage, 
+    increaseUsage, 
+    RedisError 
+} = require("../store");
 
 const verifyUsage = async (req, res, next) => {
+    if (!req.user) {
+        return res.status(400).json({ error: "invalid payload" });
+    }
+
+    const { userId, role } = req.user;
+
+    if (!userId || !role) {
+        return res.status(400).json({ error: 'invalid_payload' });
+    }
+
     try {
-        const { userId, role } = req.user;
+        if (role === "premium") {
+            next();
+        } else {
+            const { usage } = await getUsage(userId) === null ? await setUsage(userId) : await increaseUsage(userId);
 
-        if (role === 'premium') next();
+            if (usage > 5) {
+                return res.status(403).json({ error: 'monthly_usage_exceed' });
+            }
 
-        const { usage } = await getUsage(userId) === null ? await setUsage(userId) : await increaseUsage(userId);
-
-        if (usage > 5) {
-            return res.status(403).json({ error: 'monthly_usage_exceed' });
+            next();
         }
 
-        next();
     } catch (error) {
         if (error instanceof RedisError) {
             return res.status(500).json({ error: error.message });
