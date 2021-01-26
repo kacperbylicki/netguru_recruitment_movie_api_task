9d1395e3cb0e08423bd86bd77e8569dd30d0fa14
Kacper Bylicki <kacperbylicki@MacBook-Pro-Kacper.local>
2021-01-26 18:05:11 +0100

    Update usage middleware to handle object of usage from Redis store middleware

--- source/middleware/usage/index.js
+++ source/middleware/usage/index.js
@@ -1,10 +1,25 @@
-const verifyUsage = async () => {
-    const { userId, role } = req.user;
+const { getUsage, setUsage, increaseUsage, RedisError } = require("../store");
 
-    if (role === 'premium') next();
+const verifyUsage = async (req, res, next) => {
+    try {
+        const { userId, role } = req.user;
 
+        if (role === 'premium') next();
 
+        const { usage } = await getUsage(userId) === null ? await setUsage(userId) : await increaseUsage(userId);
 
+        if (usage > 5) {
+            return res.status(403).json({ error: 'monthly_usage_exceed' });
+        }
+
+        next();
+    } catch (error) {
+        if (error instanceof RedisError) {
+            return res.status(500).json({ error: error.message });
+        }
+
+        next(error);
+    }
 };
 
 module.exports = {
