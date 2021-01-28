83dd612b899e313e95ca80ff14bb9761f4277de3
Kacper Bylicki <kacperbylicki@MacBook-Pro-Kacper.local>
2021-01-28 20:55:08 +0100

    Add testing database

--- source/database/pool/index.js
+++ source/database/pool/index.js
@@ -1,11 +1,19 @@
 const { Pool } = require("pg");
 
+const { 
+    NODE_ENV,
+    POSTGRES_HOST,
+    POSTGRES_TEST_HOST,
+    POSTGRES_PORT,
+    POSTGRES_TEST_PORT
+} = process.env; 
+
 const pool = new Pool({
     user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
+    host: NODE_ENV === 'test' ? POSTGRES_HOST : POSTGRES_TEST_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
+    port: NODE_ENV === 'test' ? POSTGRES_PORT : POSTGRES_TEST_PORT,
 });
 
 module.exports = {
